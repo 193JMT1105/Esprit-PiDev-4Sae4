cc3e03d1c5ca5355f76aed04d91f174f96e13381
mahdi <mahdi@192.168.0.15>
2021-03-03 22:49:09 +0100

    mahdi repo commit

---
+++ src/main/java/tn/esprit/spring/repository/CostomerRepository.java
@@ -0,0 +1,10 @@
+package tn.esprit.spring.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import tn.esprit.spring.entity.Costomer;
+@Repository
+public interface CostomerRepository extends CrudRepository<Costomer, Long> {
+
+}
