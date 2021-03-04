0329f836c3769cdf17eb4bb951f087fdfca52ab7
mahdi <mahdi@192.168.0.15>
2021-03-04 02:08:25 +0100

    mahdi

--- src/main/java/tn/esprit/spring/restController/AdminRestController.java
+++ src/main/java/tn/esprit/spring/restController/AdminRestController.java
@@ -1,15 +1,20 @@
 package tn.esprit.spring.restController;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import tn.esprit.spring.service.Admin_Service;
 
 @RestController
 @RequestMapping("/Admin")
 public class AdminRestController {
-	@Autowired
-	Admin_Service adminServ;
 
+	
+	
+	
+	
+	
 }
--- src/main/java/tn/esprit/spring/restController/UserRestController.java
+++ src/main/java/tn/esprit/spring/restController/UserRestController.java
@@ -1,9 +1,19 @@
 package tn.esprit.spring.restController;
 
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import tn.esprit.spring.entity.User;
 import tn.esprit.spring.service.User_Service;
 
 @RestController
@@ -11,6 +21,32 @@ import tn.esprit.spring.service.User_Service;
 public class UserRestController {
 	@Autowired
 	User_Service usrServ ;
-	
 
+	
+	@PostMapping("/adduser")
+	public void adduser(@RequestBody  User user)
+	{
+		usrServ.addUser(user);	
+	}
+	@GetMapping("/retrieveAllusers")
+	public List<User> retrieveAllUsers()
+	{
+		return usrServ.retrieveAllUsers();
+	}
+	
+	@PutMapping("/updateUser")
+	public void updateUser(@RequestBody  User user)
+	{
+		usrServ.updateUser(user);
+	}
+	@GetMapping("/retrieveUserById/{id}")
+	public void retrieveUserById(@PathParam("id") Long id)
+	{
+		usrServ.retrieveUserById(id);
+	}
+   @DeleteMapping("/deleteUser")
+   public void deleteUser(@RequestBody User user)
+   {
+	   usrServ.deleteUser(user);  
+   }
 }
